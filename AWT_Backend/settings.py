0846b107f3f8c8ca3d0e8e47fcd8468b72582085
angelnagaba <angelnagaba99@gmail.com>
2021-12-08 11:43:03 +0300

    pulled database setting down2

--- AWT_Backend/settings.py
+++ AWT_Backend/settings.py
@@ -159,7 +159,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
 
 
 django_heroku.settings(locals())
@@ -175,3 +175,4 @@ DATABASES = {
         'PORT': '',
     }
 }
+DATABASES['default'].update(prod_db)
