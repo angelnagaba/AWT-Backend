1e1b3c0928e3ea99331682134e2a6efdc753219f
angelnagaba <angelnagaba99@gmail.com>
2021-12-10 12:56:21 +0300

    worked on the login api requirements.txt

--- caretaker/urls.py
+++ caretaker/urls.py
@@ -18,6 +18,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/login/', LoginAPI.as_view(), name='login'),
     path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
-    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
 
